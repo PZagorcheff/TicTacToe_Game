e3b8b6d5e5e5a655699c0e667f9b0a70f5fa8473
Petar Zagorchev <petar.zagorcheff@gmail.com>
2024-10-26 21:58:05 +0100

    Update javascript.js
    - Start coding the JS functions.
    - Start with first function - placeXOrO().

--- src/src/js/javascript.js
+++ src/src/js/javascript.js
@@ -1,3 +1,32 @@
-let activePlayer = 'X';
 
-let selectedSquares = []; //This array stores an array of moves. Also used to determine win conditions.
+let activePlayer = 'X';
+let selectedSquares = [];
+
+function placeXOrO(squareNumber) {
+
+    if (!selectedSquares.some(element => element.includes(squareNumber))) {
+        let select = document.getElementById(squareNumber);
+		
+        //Checks who's turn it is
+        if (activePlayer === 'X') {
+            select.style.backgroundImage = 'url("assets/images/x.png")';
+        } else {
+            select.style.backgroundImage = 'url("assets/images/o.png")';
+        }
+        selectedSquares.push(squareNumber + activePlayer);
+		
+        //Changing the active player.
+        if (activePlayer === 'X') {
+            activePlayer = 'O';
+        } else {
+            activePlayer = 'X';
+        }
+		
+        //Plays placement sound.
+        audio('./assets/audio/turn_place.wav');
+        if (activePlayer === 'O') {
+			...
+        }
+        return true;
+    }
+	...
