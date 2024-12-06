dcd7ca4c7534f2673fdcb5131296ae16487192af
Petar Zagorchev <petar.zagorcheff@gmail.com>
2024-12-06 19:02:57 +0000

    Update javascript.js
    - Add nested function animateLineDrawing().

--- src/src/js/javascript.js
+++ src/src/js/javascript.js
@@ -103,4 +103,29 @@ function drawWinLine(coordX1, coordY1, coordX2, coordY2) {
         y2 = coordY2,
         x = x1,
         y = y1;
+		
+    function animateLineDrawing() {
+        
+        const animationLoop = requestAnimationFrame(animateLineDrawing);
+
+        c.clearRect(0, 0, 608, 608);
+        c.beginPath();
+        c.moveTo(x1, y1);
+        c.lineTo(x, y);
+        c.lineWidth = 10;
+        c.strokeStyle = 'rgba(70, 255, 33, .8)';
+        c.stroke();
+
+        if (x1 <= x2 && y1 <= y2) {
+            if (x < x2) { x += 10; }
+            if (y < y2) {y += 10; }
+            if (x >= x2 && y >= y2) { cancelAnimationFrame(animationLoop); }
+        }
+		
+        if (x1 <= x2 && y1 >= y2) {
+            if (x < x2) { x += 10; }
+            if (y > y2) { y -= 10; }
+            if (x >= x2 && y <= y2) { cancelAnimationFrame(animationLoop); }
+        }
+    }
 }
